23d6021ad3ef8219a7c4e138018209d398dbd714
alexander <cybsloth@gmail.com>
2021-02-16 13:56:52 +0300

    Fixes and extend autouplift functionality

--- lightautoml/addons/uplift/base.py
+++ lightautoml/addons/uplift/base.py
@@ -1,5 +1,5 @@
 import abc
-from typing import Any, Dict, Optional, Tuple, Type
+from typing import Any, Dict, Iterable, List, Optional, Tuple, Type
 
 from log_calls import record_history
 import numpy as np
@@ -12,6 +12,7 @@ from lightautoml.addons.uplift.meta_learners import MetaLearner, TLearner, T2Lea
 from lightautoml.addons.uplift import meta_learners as uplift_meta_learners
 from lightautoml.addons.uplift import metrics as uplift_metrics
 from lightautoml.addons.uplift import utils as uplift_utils
+from lightautoml.report.report_deco import ReportDecoUplift
 
 
 @record_history(enabled=False)
@@ -31,11 +32,9 @@ class AutoUplift(BaseAutoUplift):
 
     Using greed-search to choose best uplift-approach.
 
-    WARNING: Currently, construct of report is not supported.
-
     """
-    __meta_learner_classes__ = (
-        TLearner, T2Learner, XLearner, RLearner,
+    __meta_learner_classes__: Iterable[Type[MetaLearner]] = (
+        TLearner, XLearner
     )
 
     def __init__(self, base_task: Task = Task('binary'), metric: str = 'adj_qini', normed_metric: bool = True,
@@ -48,14 +47,19 @@ class AutoUplift(BaseAutoUplift):
             test_size: Size of test part, which use for
 
         """
-        self.best_meta_learner: MetaLearner
+        assert 0.0 < test_size < 1.0, "'test_size' must be between (0.0, 1.0)"
+
+        self.best_meta_learner: Optional[MetaLearner] = None
         self.metric = metric
         self.normed_metric = normed_metric
         self.test_size = test_size
         self.base_task = base_task
+        self.holdout_metrics: List[float] = []
 
     def fit(self, data: DataFrame, roles: Dict):
-        """Fit meta-learner
+        """Fit AutoUplift
+
+        Choose best metalearner and fit it.
 
         Args:
             train_data: Dataset to train
@@ -71,7 +75,7 @@ class AutoUplift(BaseAutoUplift):
         test_treatment = test_data[treatment_col].ravel()
         test_target = test_data[target_col].ravel()
 
-        best_meta_learner: Optional[uplift_meta_learners.MetaLearner] = None
+        best_meta_learner: Optional[MetaLearner] = None
         max_metric_value = 0.0
 
         for meta_learner_class in self.__meta_learner_classes__:
@@ -79,7 +83,9 @@ class AutoUplift(BaseAutoUplift):
             meta_learner.fit(train_data, roles)
             uplift_pred, _, _ = meta_learner.predict(test_data)
 
-            metric_value = uplift_metrics.calculate_uplift_auc(test_target, uplift_pred.ravel(), test_treatment, self.metric)
+            metric_value = uplift_metrics.calculate_uplift_auc(test_target, uplift_pred.ravel(), test_treatment, self.metric,
+                self.normed_metric)
+            self.holdout_metrics.append(metric_value)
 
             if best_meta_learner is None:
                 best_meta_learner = meta_learner
@@ -92,6 +98,8 @@ class AutoUplift(BaseAutoUplift):
     def predict(self, data: DataFrame) -> Tuple[np.ndarray, ...]:
         """Predict treatment effects
 
+        Predict treatment effects using best metalearner
+
         Args:
             data: Dataset to perform inference.
 
@@ -100,4 +108,37 @@ class AutoUplift(BaseAutoUplift):
             ...: None or predictions of base task values on treated(control)-group
 
         """
+        assert self.best_meta_learner is not None, "First call 'self.fit(...), to choose best metalearner"
+
         return self.best_meta_learner.predict(data)
+
+    def create_best_meta_learner(self) -> ReportDecoUplift:
+        """ Create best metalearner with report functionality
+
+        Returns:
+            metalearner_deco: Best metalearner is wrapped by ReportDecoUplift
+
+        """
+        assert self.best_meta_learner is not None, "First call 'self.fit(...), to choose best metalearner"
+
+        rdu = ReportDecoUplift()
+        metalearner_deco = rdu(type(self.best_meta_learner)(base_task=self.base_task))
+        return metalearner_deco
+
+    def get_metalearners_ranting(self) -> DataFrame:
+        """Get rating of metalearners
+
+        Returns:
+            rating_table: DataFrame with rating
+
+        """
+        rating_table = DataFrame({
+            'MetaLearner': list(map(lambda c: c.__name__, self.__meta_learner_classes__)),
+            'Metrics': self.holdout_metrics
+        })
+
+        rating_table['Rank'] = rating_table['Metrics'].rank(method='first')
+        rating_table.sort_values('Rank', ascending=False, inplace=True)
+        rating_table.reset_index(drop=True, inplace=True)
+
+        return rating_table
