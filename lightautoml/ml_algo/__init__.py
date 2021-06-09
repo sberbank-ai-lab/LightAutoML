e8cfa065d6381d85400e3ebcd13ccc025dd8910c
Alex Ryzhkov <alexmryzhkov@gmail.com>
2021-06-09 18:52:22 +0000

    Add random forest algo

--- lightautoml/ml_algo/__init__.py
+++ lightautoml/ml_algo/__init__.py
@@ -1,3 +1,3 @@
 """Modules with machine learning algorithms and hyperparameters tuning tools."""
 
-__all__ = ['tuning', 'base', 'boost_lgbm', 'boost_cb', 'linear_sklearn', 'dl_model', 'whitebox', 'utils']
+__all__ = ['tuning', 'base', 'boost_lgbm', 'boost_cb', 'random_forest', 'linear_sklearn', 'dl_model', 'whitebox', 'utils']
