9abc96c4505f688b5128e58222085f57ac374dab
Alexander Kirilin <adkirilin@gmail.com>
2021-09-16 13:45:06 +0000

    Remove redundant validatations

--- lightautoml/image/__init__.py
+++ lightautoml/image/__init__.py
@@ -1,9 +1,4 @@
 """Provides an internal interface for working with image features."""
 
-from lightautoml.utils.installation import __validate_extra_deps
-
-
-__validate_extra_deps("cv")
-
 
 __all__ = ["image"]
