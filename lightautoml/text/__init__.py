b975422b6c4beb6bcae78b55393a1b6874e9ebe4
Alexander Kirilin <adkirilin@gmail.com>
2021-09-16 13:45:06 +0000

    Remove redundant validatations

--- lightautoml/text/__init__.py
+++ lightautoml/text/__init__.py
@@ -1,10 +1,5 @@
 """Provides an internal interface for working with text features."""
 
-from lightautoml.utils.installation import __validate_extra_deps
-
-
-__validate_extra_deps("nlp")
-
 
 __all__ = [
     "tokenizer",
