99bc7221172431239f414565e3d4dafbdbb13a7e
VaBun <va.bunakov@gmail.com>
2021-10-07 20:53:06 +0000

    MonitoringDeco added

--- lightautoml/report/__init__.py
+++ lightautoml/report/__init__.py
@@ -1,13 +1,6 @@
 """Report generators and templates."""
 
-from lightautoml.utils.installation import __validate_extra_deps
+from .report_deco import ReportDeco, ReportDecoWhitebox, ReportDecoNLP
+from .monitoring_deco import MonitoringDeco
 
-from .report_deco import ReportDeco
-from .report_deco import ReportDecoNLP
-from .report_deco import ReportDecoWhitebox
-
-
-__validate_extra_deps("pdf")
-
-
-__all__ = ["ReportDeco", "ReportDecoWhitebox", "ReportDecoNLP"]
+__all__ = ['ReportDeco', 'ReportDecoWhitebox', 'ReportDecoNLP', 'MonitoringDeco']
