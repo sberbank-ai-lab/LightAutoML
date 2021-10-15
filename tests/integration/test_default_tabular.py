53416b52b36b9a8e4a8f476859fe39c356f3d32c
Rinchin <57899558+dev-rinchin@users.noreply.github.com>
2021-10-15 14:33:25 +0300

    Update test_default_tabular.py

--- tests/integration/test_default_tabular.py
+++ tests/integration/test_default_tabular.py
@@ -10,15 +10,15 @@ from lightautoml.tasks import Task
 
 @pytest.mark.integtest
 def test_default_tabular():
-  # load and prepare data
-  data = pd.read_csv("./data/sampled_app_train.csv")
-  train_data, test_data = train_test_split(data, test_size=0.2, stratify=data["TARGET"], random_state=42)
+    # load and prepare data
+    data = pd.read_csv("./data/sampled_app_train.csv")
+    train_data, test_data = train_test_split(data, test_size=0.2, stratify=data["TARGET"], random_state=42)
 
-  # run automl
-  automl = TabularAutoML(task=Task("binary"))
-  oof_predictions = automl.fit_predict(train_data, roles={"target": "TARGET", "drop": ["SK_ID_CURR"]})
-  te_pred = automl.predict(test_data)
+    # run automl
+    automl = TabularAutoML(task=Task("binary"))
+    oof_predictions = automl.fit_predict(train_data, roles={"target": "TARGET", "drop": ["SK_ID_CURR"]})
+    te_pred = automl.predict(test_data)
 
-  # calculate scores
-  print(f"Score for out-of-fold predictions: {roc_auc_score(train_data['TARGET'].values, oof_predictions.data[:, 0])}")
-  print(f"Score for hold-out: {roc_auc_score(test_data['TARGET'].values, te_pred.data[:, 0])}")
+    # calculate scores
+    print(f"Score for out-of-fold predictions: {roc_auc_score(train_data['TARGET'].values, oof_predictions.data[:, 0])}")
+    print(f"Score for hold-out: {roc_auc_score(test_data['TARGET'].values, te_pred.data[:, 0])}")
