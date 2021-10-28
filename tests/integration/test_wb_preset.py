c18e385b3ff92be67a7dea97ba5dc1ebb7c71aef
dev-rinchin <rinchin.damdinov@yandex.ru>
2021-10-28 11:22:12 +0000

    fix test_wb_preset

--- tests/integration/test_wb_preset.py
+++ tests/integration/test_wb_preset.py
@@ -1,17 +1,14 @@
-import pandas as pd
 import pytest
 
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split
 
 from lightautoml.automl.presets.whitebox_presets import WhiteBoxPreset
 
 
 @pytest.mark.integtest
-def tests_wb_preset(binary_task):
+def tests_wb_preset(jobs_train_test, binary_task):
     # load and prepare data
-    data = pd.read_csv("../../examples/data/jobs_train.csv")
-    train, test = train_test_split(data.drop("enrollee_id", axis=1), test_size=0.2, stratify=data["target"])
+    train, test = jobs_train_test
 
     # run automl
     automl = WhiteBoxPreset(binary_task)
